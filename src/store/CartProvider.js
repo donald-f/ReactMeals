4f396b5e82450cdd6419b1dc26690d5592d3c57a
donald-f <donald.flobeck@gmail.com>
2022-10-28 19:50:32 -0400

    Everything now works in the application! All that is left to do is add a cart animation.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -2,18 +2,61 @@ import CartContext from './cart-context';
 import React, { useReducer } from 'react';
 
 const cartReducer = (state, action) => {
+  let updatedItems;
+
   switch (action.type) {
-    case 'ADD':
-      const updatedItems = state.items.concat(action.item); // concat is a nifty way to keep things immutable (as opposed to using push())
-      // console.log(updatedItems);
+    case 'ADD': {
       const updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(action.item); // concat is a nifty way to keep things immutable (as opposed to using push())
+        // console.log(updatedItems);
+      }
       return {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
       };
-    case 'REMOVE':
-      return state;
+    }
+    case 'REMOVE': {
+      const existingCartItemIndex = state.items.findIndex((item) => {
+        return item.id === action.id;
+      });
+      const existingCartItem = state.items[existingCartItemIndex];
+      const updatedTotalAmount = state.totalAmount - existingCartItem.price;
+      if (existingCartItem.amount === 1) {
+        updatedItems = state.items.filter(
+          (item) => item.id !== existingCartItem.id
+        );
+      } else {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: --existingCartItem.amount,
+        };
+        updatedItems = state.items.map((item, i) => {
+          return i === existingCartItemIndex ? updatedItem : item;
+          /*
+          alternative to the map above could have been (teacher's solution):
+          updatedItems = [...state.items];
+          updatedItems[existingCartItemIndex] = updatedItem;
+          */
+        });
+      }
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
     default:
       return state;
   }
